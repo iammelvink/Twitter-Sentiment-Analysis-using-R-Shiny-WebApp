bfd2499e61bf465ad847535a5b4493317eff20a3
Melvin Kisten <iammelvink@gmail.com>
2021-06-11 02:57:08 +0200

    refactored for Tourism indicators

---
+++ utils/loadpackages.R
@@ -0,0 +1,9 @@
+new_libraries <- scan('requirements.txt', what = "")
+packages_to_be_installed <-
+  new_libraries[!new_libraries %in% installed.packages()[, 'Package']]
+if (length(packages_to_be_installed) != 0) {
+  install.packages(packages_to_be_installed, repos = "http://cran.us.r-project.org")
+} else {
+  lapply(new_libraries, library, character.only = TRUE)
+}
+rm(packages_to_be_installed)
